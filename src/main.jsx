e5f83ed0d1298ff4094d06458874159a3d7e5688
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-15 07:17:54 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from './layout/MainLayout/MainLayout';
+import Home from './components/Home/Home';
+import AddCoffee from './components/AddCoffee/AddCoffee';
+import UpdateCoffee from './components/UpdateCoffee/UpdateCoffee'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+        loader: () => fetch('http://localhost:3000/coffee')
+      },
+      {
+        path: "/addCoffee",
+        element: <AddCoffee></AddCoffee>
+      },
+      {
+        path: "/updateCoffee/:id",
+        element: <UpdateCoffee></UpdateCoffee>,
+        loader: ({ params }) => fetch(`http://localhost:3000/coffee/${params.id}`)
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
